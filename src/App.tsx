23c5e8cce2836a750bd1280d18515a29f2d3504c
raibove <shwetakale144@gmail.com>
2023-04-20 11:48:00 +0530

    add input and convert it to array of number

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,31 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [inputArray, setInputArray] = useState<number[]>([])
+  const [inputValue, setInputValue] = useState("")
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>)=>{
+    setInputValue(event.target.value)
+  }
+
+  const handleSubmit = ()=>{
+    const values = inputValue.split(',').map(value=> parseInt(value.trim()))
+
+    if (values.some((value) => isNaN(value) || value <= 0)) {
+      alert("Invalid input! Please enter a comma-separated list of positive integers. eg. 1,2,3");
+      return;
+    }
+
+    setInputArray(values)
+  }
 
   return (
     <div className="App">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <input type="text" value={inputValue} onChange={handleChange} placeholder="Enter comma seperated integers"/>
+        <button type="submit" onClick={handleSubmit}>Submit</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
