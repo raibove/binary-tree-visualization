8651a313bf0fd132e5bb179e2aad26338c2730d0
raibove <shwetakale144@gmail.com>
2023-04-20 15:16:48 +0530

    add canvas to draw binary tree

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import "./App.css";
 
 type Node = {
@@ -8,6 +8,7 @@ type Node = {
 };
 
 function App() {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
   const [inputArray, setInputArray] = useState<number[]>([]);
   const [inputValue, setInputValue] = useState("");
 
@@ -29,20 +30,76 @@ function App() {
 
     // Build the binary tree
     const binaryTree = buildBinaryTree(values);
-    console.log(binaryTree)
-    
+
+    // Draw the binary tree on the canvas
+    const canvas = canvasRef.current;
+    if (canvas) {
+      const context = canvas.getContext("2d");
+      if (context) {
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        drawBinaryTree(context, binaryTree, canvas.width / 2, 50, 80, 80);
+      }
+    }
   };
 
   const buildBinaryTree = (values: number[], i: number = 0): Node | null => {
     if (i >= values.length || values[i] === null) {
       return null;
     }
-  
+
     const node: Node = { value: values[i], left: null, right: null };
     node.left = buildBinaryTree(values, 2 * i + 1);
     node.right = buildBinaryTree(values, 2 * i + 2);
-  
+
     return node;
+  };
+
+  const drawBinaryTree = (
+    context: CanvasRenderingContext2D,
+    node: Node | null,
+    x: number,
+    y: number,
+    dx: number,
+    dy: number
+  ) => {
+    if (node === null) {
+      return;
+    }
+
+    const radius = 20;
+    const centerX = x + radius;
+    const centerY = y + radius;
+
+    context.beginPath();
+    context.fillStyle = "white";
+    context.arc(centerX, centerY, radius, 0, 2 * Math.PI);
+    context.strokeStyle = "black";
+    context.stroke();
+    context.fillStyle = "black";
+    context.fillText(node.value.toString(), centerX, centerY);
+
+    if (node.left !== null) {
+      const leftX = x - dx + radius;
+      const leftY = y + dy;
+
+      context.beginPath();
+      context.moveTo(centerX, centerY + radius);
+      context.lineTo(leftX, leftY);
+      context.stroke();
+
+      drawBinaryTree(context, node.left, x - dx, y + dy, dx / 2, dy);
+    }
+    if (node.right !== null) {
+      const rightX = x + dx + radius;
+      const rightY = y + dy;
+
+      context.beginPath();
+      context.moveTo(centerX, centerY + radius);
+      context.lineTo(rightX, rightY);
+      context.stroke();
+
+      drawBinaryTree(context, node.right, x + dx, y + dy, dx / 2, dy);
+    }
   };
 
   return (
@@ -57,6 +114,7 @@ function App() {
         <button type="submit" onClick={handleSubmit}>
           Submit
         </button>
+        <canvas ref={canvasRef} width={600} height={400} />
       </div>
     </div>
   );
